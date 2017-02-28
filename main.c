3b37d5398bfb6f62b0ba94852198862a1121cb82
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-02-28 11:16:57 -0700

    Created necessary methods

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include "foo.h"  /* Include the header here, to obtain the function declaration */
 
+void userInterface();
+
 int main(void)
 {
-    int y = foo(3);  /* Use the function here */
-    printf("%d\n", y);
+
+    userInterface();
+    return 0;
+}
+
+void userInterface(){
+    printf("In the userInterface method\n");
+}
+int readFile(){
+    return 0;
+}
+int writeFile(){
+    return 0;
+}
+int searchEmployee(){
+    return 0;
+}
+int insertEmployee(){
+    return 0;
+}
+int deleteEmployee(){
     return 0;
 }
