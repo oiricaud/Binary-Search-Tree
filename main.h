a26a63b9d7c4fbfcbc10ac5ca1bdfff5112e5374
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-03-04 22:33:13 -0700

    For some reason Github is saying this file is c++.

--- main.h
+++ main.h
@@ -1,15 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void userInterface();
-void readFile();
-void writeFile();
-void createBST();
-void deleteEmployee();
-void nextStateNoTreeOption();
-
-void nextState(node *root);
-void clearTextFile();
-void addUpdatedEmployees(node *root);
-void writeUpdatedEmployeesInTextFile();
 #endif //MAIN_H
